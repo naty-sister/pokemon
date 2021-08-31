d25e89ff176852d292c25dd1b4f4b887309a8f25
Natalia <natalia.ribeiro2@hotmail.com>
2021-08-31 15:43:54 -0300

    alteração

---
+++ src/app/pages/datails/datails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datails',
+  templateUrl: './datails.component.html',
+  styleUrls: ['./datails.component.scss']
+})
+export class DatailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
